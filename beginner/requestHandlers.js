14fa2c3ee07864dedb466489958f48d3942d0127
real <32797392+4558206@users.noreply.github.com>
2018-10-23 20:49:05 +0800

    post

--- beginner/requestHandlers.js
+++ beginner/requestHandlers.js
@@ -1,8 +1,9 @@
 'use strict';
 
-var exec = require("child_process").exec;
+//var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>' +
@@ -23,10 +24,10 @@ function start(response) {
     response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent: " + querystring.parse(postData).text);
     response.end();
 }
 
